f197408672021fdec620f8751d2a5c3ac917f90c
Pedro Cristo <pcristo35@gmail.com>
2023-01-07 16:29:22 +0000

    Setup project and add header component

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export default function Header() {
+    return (
+      
+        <header className="header">
+            <div className="title">
+            <img src="../../../../public/images/troll.png" alt="" />
+            <h3>Meme Generator</h3>
+            </div>
+            <p>React Course - Project 3</p>
+        </header>
+    )
+}
